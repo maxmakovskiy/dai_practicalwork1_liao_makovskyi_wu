8a81b98965e668c37d61c0417147de49bb0eb413
FeliciaCoding <49447678+FeliciaCoding@users.noreply.github.com>
2025-10-09 16:40:02 +0200

    Init+Variables

---
+++ src/main/java/ch/heigvd/DSparseMatrixLIL.java
@@ -0,0 +1,16 @@
+package ch.heigvd;
+
+import java.util.ArrayList;
+
+public class DSparseMatrixLIL {
+
+    private int nRows; // DocID
+    private int nCols; // WordID
+    private ArrayList<ArrayList<Integer>> indices; // column positions
+    private ArrayList<ArrayList<Double>>  scores;
+
+
+
+
+
+}
