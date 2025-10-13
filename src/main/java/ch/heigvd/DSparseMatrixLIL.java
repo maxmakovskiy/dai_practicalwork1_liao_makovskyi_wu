016fdb4d153cbf31cfe750945bba070f0bf7ee0f
FeliciaCoding <49447678+FeliciaCoding@users.noreply.github.com>
2025-10-13 14:29:24 +0200

    Add DSparseMatrixLIL(ArrayList<String> matrixRows)

--- src/main/java/ch/heigvd/DSparseMatrixLIL.java
+++ src/main/java/ch/heigvd/DSparseMatrixLIL.java
@@ -1,8 +1,9 @@
 package ch.heigvd;
 
+// https://docs.oracle.com/javase/8/docs/technotes/guides/collections/overview.html
 import java.util.ArrayList;
 
-
+// ref: https://matteding.github.io/2019/04/25/sparse-matrices/
 public class DSparseMatrixLIL {
 
     private int nRows; // DocID
@@ -33,7 +34,6 @@ public class DSparseMatrixLIL {
     public double get(int rowIdx, int colIdx) {
 
         if (rowIdx < 0 || rowIdx >= nRows || colIdx < 0 || colIdx >= nCols){
-
             // ref : https://stackoverflow.com/questions/7312767/manually-adding-indexoutofbounds-exception
             throw new IndexOutOfBoundsException(
                 "Cannot find indices for rowIdx: " + rowIdx + " and colIdx: " + colIdx
@@ -81,8 +81,109 @@ public class DSparseMatrixLIL {
 
     }
 
+    private static int parseNRows(ArrayList<String> matrixRows) {
+
+        if (matrixRows == null || matrixRows.isEmpty()){
+            throw new IndexOutOfBoundsException("Invalid matrix rows!");
+        }
+
+        String nRowStr = matrixRows.get(0);
+        String[] nRowStrList = nRowStr.split(":");
+        return Integer.parseInt(nRowStrList[1].trim());
+    }
+
+    private static int parseNCols(ArrayList<String> matrixRows) {
+
+        if (matrixRows.size() < 2 ){
+            throw new IndexOutOfBoundsException("Invalid matrix rows!");
+        }
+
+        String nColStr = matrixRows.get(1);
+        String[] nColStrList = nColStr.split(":");
+        return Integer.parseInt(nColStrList[1].trim());
+    }
+
     // Construct matrix from one long string or array of strings
-    // public DSparseMatrixLIL(ArrayList<String> matrixRows)
+     public DSparseMatrixLIL(ArrayList<String> matrixRows){
+
+        // call DSparseMatrixLIL(int nRows, int nCols)
+        this(parseNRows(matrixRows), parseNCols(matrixRows));
+
+        if (matrixRows.size() < 3){
+            throw new IndexOutOfBoundsException("Invalid matrix rows!");
+        }
+
+
+/*
+        int lineIdx = 0;
+
+        // "nRows : 3"
+        String nRowStr = matrixRows.get(lineIdx++);
+        String[] nRowStrList = nRowStr.split(":");
+        this.nRows = Integer.parseInt(nRowStrList[1].trim());
+
+        //  "nCols : 14"
+         String nColStr = matrixRows.get(lineIdx++);
+         String[] nColStrList = nColStr.split(":");
+         this.nCols = Integer.parseInt(nColStrList[1].trim());
+*/
+
+         //DSparseMatrixLIL(nRows, nCols);
+
+        //  "Indices",
+         //    "0 : 0, 6, 7, 9, 13",
+         //    "1 : 0, 1, 2, 8, 11, 12",
+         //    "2 : 3, 4, 5, 7, 10",
+
+
+         int lineIdx = 2;
+
+         // Skip  "Indices",
+         lineIdx++;
+
+         for (int i =0; i < nRows; i++) {
+             String docLine = matrixRows.get(lineIdx++);
+             String[] docLine2StrList = docLine.split(":");
+
+             if (docLine2StrList.length > 1) {
+
+                 String indicesStr = docLine2StrList[1].trim();
+
+                if (!indicesStr.isEmpty()){
+                    String[] indicesStrList = indicesStr.split(",");
+
+                    for (String idxStr : indicesStrList) {
+                        int idxInt = Integer.parseInt(idxStr.trim());
+                        indices.get(i).add(idxInt);
+                    }
+                }
+             }
+         }
+
+         // skip "Data"
+         lineIdx++;
+
+         for (int i =0; i < nRows; i++) {
+             String docLine = matrixRows.get(lineIdx++);
+             String[] docLine2StrList = docLine.split(":");
+
+             if (docLine2StrList.length > 1) {
+
+                 String scoresStr = docLine2StrList[1].trim();
+
+                 if (!scoresStr.isEmpty()) {
+                     String[] scoresStrList = scoresStr.split(",");
+
+                     for (String scoreStr : scoresStrList) {
+                         int scoreInt = Integer.parseInt(scoreStr.trim());
+                         indices.get(i).add(scoreInt);
+                     }
+
+                 }
+             }
+         }
+
+     }
     // public DSparseMatrixLIL(String matrix)
 
     // public String toString()
