759fae508b75a56d1ad5734623cd7801da0872e2
AlterSpectre <guoyu.wu.01@gmail.com>
2025-10-15 22:29:35 +0200

    add description for each command

--- src/main/java/ch/heigvd/commands/Search.java
+++ src/main/java/ch/heigvd/commands/Search.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 
 @Command(
         name = "search",
+        description = "- search through your document using index file",
         mixinStandardHelpOptions = true
 )
 public class Search implements Runnable {
