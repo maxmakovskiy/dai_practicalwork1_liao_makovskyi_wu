b0619dbeae9c40dd8cb5ba4a9cf53d9bbdde99b2
AlterSpectre <guoyu.wu.01@gmail.com>
2025-10-15 08:27:35 +0200

    add comment and javadoc

--- src/main/java/ch/heigvd/bm25/utils/Index.java
+++ src/main/java/ch/heigvd/bm25/utils/Index.java
@@ -1,17 +1,15 @@
 package ch.heigvd.bm25.utils;
 
 import ch.heigvd.bm25.exceptions.IndexException;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class Index {
-    public DSparseMatrixLIL matrix;
-
-    private ArrayList<String> vocabulary;
-    private ArrayList<String> documentNames;
-    private int numOfDocs;
-    private int vocabSize;
+    public DSparseMatrixLIL matrix; // matrix of score
+    private ArrayList<String> vocabulary; //  list of words present
+    private ArrayList<String> documentNames; // list of file name
+    private int numOfDocs; // number of file saved
+    private int vocabSize; // size of vocabulary
 
     public Index(int vocabSize, int numOfDocs, ArrayList<String> vocab, ArrayList<String> documentNames){
         this.vocabSize = vocabSize;
@@ -21,10 +19,15 @@ public class Index {
         this.matrix = new DSparseMatrixLIL(numOfDocs, vocabSize);
     }
 
+    /**
+     * create an Index object from a string
+     * @param stringIndex Index as a string
+     * @return a Index object build from a string
+     */
     public static Index importIndex(String stringIndex) {
-        int numOfDocs, vocabSize;
+        int numOfDocs; // number of document file
+        int vocabSize; // size of all words present
 
-        // create an Index with a string
         // separate each line in the table
         ArrayList<String> tableStringIndex = new ArrayList<>(
                 List.of(stringIndex.split("\n")));
@@ -66,31 +69,56 @@ public class Index {
         return index;
     }
 
+    /**
+     * get numOfDocs
+     * @return the number of document file saved in Index
+     */
     public int getNumOfDocs(){
         return numOfDocs;
     }
+
+    /**
+     * get the vocabulary size
+     * @return vocabulary size
+     */
     public int getVocabSize(){
         return vocabSize;
     }
+
+    /**
+     * get the vocabulary
+     * @return arrayList of the vocabulary present
+     */
     public ArrayList<String> getVocabulary() {
         return vocabulary;
     }
+
+    /**
+     * get the name of document fron its id
+     * @param docIdx index of the document file
+     * @return name of the document file from id docIdx
+     */
     public String getDocumentName(int docIdx) {
         return this.documentNames.get(docIdx);
     }
 
-    // reuses matrix.toString()
+    /**
+     * convert Index object into a string
+     * @return a string fromat of Index object
+     */
     public String toString(){
+
+        // StringBuilder allow us to build string faster
         StringBuilder result = new StringBuilder();
 
         result.append("docNames\n");
         result.append(String.join(" ", documentNames));
 
         result.append("\nnumOfDocs\n");
-        result.append(this.numOfDocs + "\n");
-        result.append("vocabSize\n");
-        result.append(this.vocabSize + "\n");
-        result.append("vocabulary\n");
+        result.append(this.numOfDocs);
+        result.append("\nvocabSize\n");
+        result.append(this.vocabSize);
+        result.append("\nvocabulary\n");
 
         result.append(String.join(" ", vocabulary));
         result.append("\nmatrixScores\n");
