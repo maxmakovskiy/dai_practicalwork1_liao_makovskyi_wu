35e1bb07b63c90031b389c2af9ce610b01624c72
maxmakovskiy <makovskiymax@gmail.com>
2025-10-20 11:56:24 +0200

    test : DSparseMatrixLIL | set & get

--- src/main/test/ch/heigvd/bm25/utils/DSparseMatrixLILTest.java
+++ src/main/test/ch/heigvd/bm25/utils/DSparseMatrixLILTest.java
@@ -22,5 +22,45 @@ public class DSparseMatrixLILTest {
         assertEquals("nRows and nCols must be non-negative.", exc.getMessage());
     }
 
+    @Test
+    void get() {
+        DSparseMatrixLIL matrix = new DSparseMatrixLIL(4, 4);
+
+        double res = matrix.get(1,1);
+
+        assertEquals(0.0, res);
+    }
+
+    @Test
+    void breakingGet() {
+        DSparseMatrixLIL matrix = new DSparseMatrixLIL(4, 4);
+        IndexOutOfBoundsException exc = assertThrows(IndexOutOfBoundsException.class,
+                () -> matrix.get(10, 4));
+
+        assertEquals("Cannot find indices for rowIdx: 10 and colIdx: 4", exc.getMessage());
+    }
+
+    @Test
+    void set() {
+        DSparseMatrixLIL matrix = new DSparseMatrixLIL(4, 4);
+
+        assertEquals(0.0, matrix.get(1,1));
+
+        double val = 4.0;
+        matrix.set(1, 1, val);
+
+        assertEquals(val, matrix.get(1,1));
+    }
+
+    @Test
+    void breakingSetOutOfBound() {
+        DSparseMatrixLIL matrix = new DSparseMatrixLIL(4, 4);
+
+        IndexOutOfBoundsException exc = assertThrows(IndexOutOfBoundsException.class,
+                () -> matrix.set(1, 10, 4.0));
+
+        assertEquals("Cannot set value at rowIdx: 1 and colIdx: 10", exc.getMessage());
+    }
+
 
 }
