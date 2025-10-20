3d8c37c6e3aa8e8e7aff2eec1f4cad8f50f88e19
maxmakovskiy <makovskiymax@gmail.com>
2025-10-20 12:28:16 +0200

    test: DSparseMatrixLIL | parsing and toString

--- src/main/test/ch/heigvd/bm25/utils/DSparseMatrixLILTest.java
+++ src/main/test/ch/heigvd/bm25/utils/DSparseMatrixLILTest.java
@@ -2,6 +2,7 @@ package ch.heigvd.bm25.utils;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import org.junit.jupiter.api.Test;
 
@@ -60,6 +61,43 @@ public class DSparseMatrixLILTest {
                 () -> matrix.set(1, 10, 4.0));
 
         assertEquals("Cannot set value at rowIdx: 1 and colIdx: 10", exc.getMessage());
+    }
+
+    @Test
+    void strToMatrix() {
+        DSparseMatrixLIL matrix = new DSparseMatrixLIL(
+            "nRows : 4\n" +
+            "nCols : 4\n" +
+            "Indices\n" +
+            "0 : 0\n" +
+            "1 : 1\n" +
+            "2 : 2\n" +
+            "3 : 3\n" +
+            "Data\n" +
+            "0 : 0.2\n" +
+            "1 : 0.42\n" +
+            "2 : 0.45\n" +
+            "3 : 0.17\n"
+        );
+
+        assertEquals(0.2, matrix.get(0, 0));
+        assertEquals(0.42, matrix.get(1, 1));
+        assertEquals(0.45, matrix.get(2, 2));
+        assertEquals(0.17, matrix.get(3, 3));
+    }
+
+    @Test
+    void matrixToStr() {
+        DSparseMatrixLIL matrix = new DSparseMatrixLIL(4, 4);
+
+        matrix.set(0, 0, 1.0);
+        matrix.set(1, 1, 2.0);
+        matrix.set(2, 2, 3.0);
+        matrix.set(3, 3, 4.0);
+
+        String res = matrix.toString();
+
+        assertNotEquals(0.0, res.length());
     }
 
 
