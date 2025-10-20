bdb93ae43828f4983cb0c15732412f51836a0f3c
maxmakovskiy <makovskiymax@gmail.com>
2025-10-20 11:28:22 +0200

    test: DSparseMatrixLIL | breaking construction of matrix

--- src/main/test/ch/heigvd/bm25/utils/DSparseMatrixLILTest.java
+++ src/main/test/ch/heigvd/bm25/utils/DSparseMatrixLILTest.java
@@ -1,6 +1,7 @@
 package ch.heigvd.bm25.utils;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
@@ -8,9 +9,18 @@ import org.junit.jupiter.api.Test;
 public class DSparseMatrixLILTest {
 
     @Test
-    void emptySparseMatrix() {
+    void constructEmptySparseMatrix() {
         DSparseMatrixLIL matrix = new DSparseMatrixLIL(4, 4);
         assertEquals(0.0, matrix.get(1, 2));
     }
+
+    @Test
+    void breakConstructionSparseMatrix() {
+        IllegalArgumentException exc = assertThrows(IllegalArgumentException.class,
+                () -> new DSparseMatrixLIL(-4, 4));
+
+        assertEquals("nRows and nCols must be non-negative.", exc.getMessage());
+    }
+
 
 }
